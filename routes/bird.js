8beddd57bb24834e6b92a248cec9fc36e599acc5
LH <lindsaynhenderson@gmail.com>
2022-10-22 21:00:55 -0500

    made routes, and controller functions for bird model

---
+++ routes/bird.js
@@ -0,0 +1,18 @@
+import { Router } from 'express'
+import * as birdCtrl from '../controllers/bird.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+
+const router = Router()
+
+/*---------- Public Routes ----------*/
+
+
+/*---------- Protected Routes ----------*/
+router.use(decodeUserFromToken)
+router.post('/', checkAuth, blogsCtrl.create)
+router.get('/', checkAuth, blogsCtrl.index)
+router.get('/:id', checkAuth, blogsCtrl.show)
+router.put('/:id', checkAuth, blogsCtrl.update)
+router.delete('/:id', checkAuth, blogsCtrl.delete)
+
+export { router }
