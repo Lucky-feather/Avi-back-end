91b3612a6ab3936c4a8c00024c82b861e64a2956
LH <lindsaynhenderson@gmail.com>
2022-10-26 10:41:19 -0500

    change routes and answer schema

--- routes/questions.js
+++ routes/questions.js
@@ -10,13 +10,12 @@ const router = Router()
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
 router.post('/', checkAuth, questionCtrl.create)
+router.post('/:id/answers', checkAuth, questionCtrl.createAnswer)
 router.get('/', checkAuth, questionCtrl.index)
 router.get('/:id', checkAuth, questionCtrl.show)
 router.put('/:id', checkAuth, questionCtrl.update)
 router.delete('/:id', checkAuth, questionCtrl.delete)
-
-router.post('/:id/answers', checkAuth, questionCtrl.createAnswer)
-router.put('/:questionId/answers/:answerId', checkAuth, questionCtrl.updateAnswer)
-router.delete('/:questionId/answers/:answerId', checkAuth, questionCtrl.deleteAnswer)
+// router.put('/:questionId/answers/:answerId', checkAuth, questionCtrl.updateAnswer)
+// router.delete('/:questionId/answers/:answerId', checkAuth, questionCtrl.deleteAnswer)
 
 export { router }
